f05f5fba0f310d2ca9f5900f6e5b288576f7ccf7
RadityaDito <raditya.ihsan@ui.ac.id>
2025-01-31 19:11:58 +0700

    Add home

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -1,67 +1,71 @@
 package handlers
 
 import (
-    "net/http"
-    "strconv"
+	"net/http"
+	"strconv"
 
-    "golang-echo-crud/models"
-    "golang-echo-crud/repositories"
+	"golang-echo-crud/models"
+	"golang-echo-crud/repositories"
 
-    "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 )
 
+func Home(c echo.Context) error {
+	return c.String(http.StatusOK, "Welcome to Golang Echo CRUD")
+}
+
 func GetUsers(c echo.Context) error {
-    users, err := repositories.GetUsers()
-    if err != nil {
-        return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
-    }
-    return c.JSON(http.StatusOK, users)
+	users, err := repositories.GetUsers()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, users)
 }
 
 func GetUserByID(c echo.Context) error {
-    id, _ := strconv.Atoi(c.Param("id"))
-    user, err := repositories.GetUserByID(uint(id))
-    if err != nil {
-        return c.JSON(http.StatusNotFound, map[string]string{"error": "User not found"})
-    }
-    return c.JSON(http.StatusOK, user)
+	id, _ := strconv.Atoi(c.Param("id"))
+	user, err := repositories.GetUserByID(uint(id))
+	if err != nil {
+		return c.JSON(http.StatusNotFound, map[string]string{"error": "User not found"})
+	}
+	return c.JSON(http.StatusOK, user)
 }
 
 func CreateUser(c echo.Context) error {
-    user := new(models.User)
-    if err := c.Bind(user); err != nil {
-        return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
-    }
-    err := repositories.CreateUser(user)
-    if err != nil {
-        return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
-    }
-    return c.JSON(http.StatusCreated, user)
+	user := new(models.User)
+	if err := c.Bind(user); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	err := repositories.CreateUser(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusCreated, user)
 }
 
 func UpdateUser(c echo.Context) error {
-    id, _ := strconv.Atoi(c.Param("id"))
-    user, err := repositories.GetUserByID(uint(id))
-    if err != nil {
-        return c.JSON(http.StatusNotFound, map[string]string{"error": "User not found"})
-    }
+	id, _ := strconv.Atoi(c.Param("id"))
+	user, err := repositories.GetUserByID(uint(id))
+	if err != nil {
+		return c.JSON(http.StatusNotFound, map[string]string{"error": "User not found"})
+	}
 
-    if err := c.Bind(&user); err != nil {
-        return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
-    }
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
 
-    err = repositories.UpdateUser(&user)
-    if err != nil {
-        return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
-    }
-    return c.JSON(http.StatusOK, user)
+	err = repositories.UpdateUser(&user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, user)
 }
 
 func DeleteUser(c echo.Context) error {
-    id, _ := strconv.Atoi(c.Param("id"))
-    err := repositories.DeleteUser(uint(id))
-    if err != nil {
-        return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
-    }
-    return c.JSON(http.StatusOK, map[string]string{"message": "User deleted"})
+	id, _ := strconv.Atoi(c.Param("id"))
+	err := repositories.DeleteUser(uint(id))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, map[string]string{"message": "User deleted"})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,16 @@
 package routes
 
 import (
-    "golang-echo-crud/handlers"
+	"golang-echo-crud/handlers"
 
-    "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 )
 
 func InitRoutes(e *echo.Echo) {
-    e.GET("/users", handlers.GetUsers)
-    e.GET("/users/:id", handlers.GetUserByID)
-    e.POST("/users", handlers.CreateUser)
-    e.PUT("/users/:id", handlers.UpdateUser)
-    e.DELETE("/users/:id", handlers.DeleteUser)
+	e.GET("/", handlers.Home)
+	e.GET("/users", handlers.GetUsers)
+	e.GET("/users/:id", handlers.GetUserByID)
+	e.POST("/users", handlers.CreateUser)
+	e.PUT("/users/:id", handlers.UpdateUser)
+	e.DELETE("/users/:id", handlers.DeleteUser)
 }
